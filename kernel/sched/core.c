4ba30fe01e62be2b8d01b3b2d2437ace67b0f1b6
Juri Lelli <juri.lelli@gmail.com>
2012-04-25 23:43:48 +0200

    use nr_cpus_allowed from the new position
    Since nr_cpus_allowed is now outside of sched/rt.c, align
    sched/dl.c to use it.

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5502,7 +5502,7 @@ void do_set_cpus_allowed(struct task_struct *p, const struct cpumask *new_mask)
 		if (p->sched_class && p->sched_class->set_cpus_allowed)
 			p->sched_class->set_cpus_allowed(p, new_mask);
 		p->nr_cpus_allowed = cpumask_weight(new_mask);
-		p->dl.nr_cpus_allowed = cpumask_weight(new_mask);
+		p->nr_cpus_allowed = cpumask_weight(new_mask);
 	}
 	cpumask_copy(&p->cpus_allowed, new_mask);
 }
